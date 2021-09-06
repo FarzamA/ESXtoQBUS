253b9db6f626fa8ce747fd90d2ebe7a0202ac00c
Farzam Ahmad <farzam135@gmail.com>
2021-09-06 15:50:21 -0400

    working on menus/notifications

--- smokes/guccismokes/fxmanifest.lua
+++ smokes/guccismokes/fxmanifest.lua
@@ -1,21 +1,16 @@
 fx_version 'adamant'
 game 'gta5'
 
-description 'devcore - by bary - discord - https://discord.gg/yNmGCyQYUY'
-credits 'Pravidla pro jednani s produkty od sluzby devcore naleznete na discordu'
+description 'guccismokes'
+credits 'GUCCIFLIPFLOPS'
 
 server_scripts {
-	--'@mysql-async/lib/MySQL.lua',
-	--'@es_extended/locale.lua',
 	'config.lua',
-	--'locales/*.lua',
 	'server/*.lua'
 }
 
 
 client_scripts {
-	--'@es_extended/locale.lua',
 	'config.lua',
-	--'locales/*.lua',
 	'client/*.lua'
 }
